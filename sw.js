384713897d1ba124d55c59fe5a66aa4a7aef11e5
marcospss <marcospss@gmail.com>
2020-05-20 12:49:19 -0300

    Correção cache offline

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const CACHE_STATIC_NAME = `static-${VERSION}`;
 const CACHE_DYNAMIC_NAME = `dynamic-${VERSION}`;
 const CACHE_SIZE = 1000;
 const OFFLINE_URL = '/offline.html';
-const assets = [
+const STATIC_FILES = [
   '/',
   '/index.html',
   '/offline.html',
@@ -15,61 +15,55 @@ const assets = [
   'https://fonts.googleapis.com/icon?family=Material+Icons',
 ];
 
-// cache size limit function
-const limitCacheSize = (name, size) => {
-  caches.open(name).then(cache => {
-    cache.keys().then(keys => {
-      if(keys.length > size){
-        cache.delete(keys[0]).then(limitCacheSize(name, size));
-      }
-    });
-  });
-};
-// https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook?hl=pt-br
-// https://developer.mozilla.org/pt-BR/docs/Web/API/Service_Worker_API/Using_Service_Workers
-// install event
-self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_STATIC_NAME);
-    await cache.addAll(assets);
-  })());
+self.addEventListener('install', function (event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function (cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll(STATIC_FILES);
+      })
+  )
 });
 
-// activate event
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-    const keysCache = await caches.keys();
-    return Promise.all(keysCache
-      .filter(key => key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME)
-      .map(key => caches.delete(key))
-    );
-  })());
-
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
 });
 
-// fetch events
-self.addEventListener('fetch', async (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(cacheRes => {
-      return cacheRes || fetch(event.request).then(fetchRes => {
-        return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
-          cache.put(event.request.url, fetchRes.clone())
-          // check cached items size
-          limitCacheSize(CACHE_DYNAMIC_NAME, CACHE_SIZE);
-          return fetchRes;
-        })
-      });
-    }).catch(async () => {
-      const cache = await caches.open(CACHE_STATIC_NAME);
-      const cachedResponse = await cache.match(OFFLINE_URL);
-      return cachedResponse;
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function(err) {
+              return caches.open(CACHE_STATIC_NAME)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
+        }
+      })
   );
 });
